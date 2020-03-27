f81953706f744c7c746b8c25b8a8e83110df27d5
Valy Romain <rom1valy@gmail.com>
2020-03-27 15:43:17 +0100

    Updates

---
+++ precache-manifest.9f33cb9e9ed2c98176e1bb8e538d68ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b210442ddd1006d12429ba659ae8d620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af1c94b523f9a596cdae",
+    "url": "/static/css/main.10bed5eb.chunk.css"
+  },
+  {
+    "revision": "12b1a611c5371b055854",
+    "url": "/static/js/2.832881ce.chunk.js"
+  },
+  {
+    "revision": "b5cd27a09ae656b97b48558c80b580fd",
+    "url": "/static/js/2.832881ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1c94b523f9a596cdae",
+    "url": "/static/js/main.8eb54a08.chunk.js"
+  },
+  {
+    "revision": "318e3365187fa673976d",
+    "url": "/static/js/runtime-main.3247507d.js"
+  }
+]);
